a836187070d9204d802d4d44bb6862b6dd50b225
Shir Ehrlich <shir.ehrlich@mail.huji.ac.il>
2022-04-07 19:02:13 +0300

    tested backend manger with simple fields

--- Autopus/autopus_main.py
+++ Autopus/autopus_main.py
@@ -4,13 +4,22 @@ end_point= "http://content.guardianapis.com/search"
 key = "92ecbb3d-1602-42b3-acdb-e0a9d166e354"
 pages = 5
 
+class Autopus:
+    def __init__(self):
+        self.UI = ""
+        self.fields = ["headline","firstPublicationDate","byline","shortUrl","bodyText"] # from ui
+        # fields, end_point, key = from UI
+        self.backend_manager =  autopus_backend_manager.CrawlerManager(self.fields,end_point,key,pages)
+        self.product = self.backend_manager.manage_crawling()
+        self.parser = ""
 
-def write_to_excel(self):
-    pass
 
+    def write_to_excel(self, path):
+        final_file = ",".join(self.fields) + "\n"
+        for art in self.product:
+            final_file+= ",".join(['"""' +fld + '"""' for fld in art ]) + "\n"
+        with open(path + ".csv", "w") as f:
+            f.write(final_file)
 
-
-manager = autopus_backend_manager.CrawlerManager(fields,end_point,key,pages)
-product = manager.manage_crawling()
-for i, art in enumerate(product):
-    print(i, " : ", art)
+aut = Autopus()
+aut.write_to_excel("test")
