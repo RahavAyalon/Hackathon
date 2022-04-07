cc98f50049a291f5ec865bd51999c0400cc7ad5c
Rahav Ayalon <rayalon@synamedia.com>
2022-04-08 02:48:27 +0300

    vfdvfdv

--- src/Button.js
+++ src/Button.js
@@ -5,15 +5,18 @@ export default function Button() {
   return (
     <>
       <div className="Buttons">
-        <button class={"btn1 linkstyle"}>
+        <button class={'btn1 linkstyle'}>
           <Link to="/filtering">Sky News</Link>
         </button>
-        <button class={"btn1 linkstyle"}>
+        <button class={'btn1 linkstyle'}>
           <Link to="/filtering">The Guardian</Link>
         </button>
         {/*<a href="something" className="button1">Button</a>*/}
-        <button class={"btn1 linkstyle"}>
-          <Link to="/filtering">Al Jazeera</Link>
+        <button class={'btn1 linkstyle'}>
+          <Link to="/filtering">
+            {' '}
+            <img src="./jazeera.jpeg" alt="my image" />
+          </Link>
         </button>
       </div>
     </>
