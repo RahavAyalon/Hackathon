ab7331c8d39daf1035d239941129478561d86cda
Orlewen <or.lewenstein@mail.huji.ac.il>
2022-04-07 23:53:05 +0300

    Edit of CSS

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,16 +11,16 @@ export default function NavbarComponent() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#link">FAQ</Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About us</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Contact us
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Links</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Help
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
