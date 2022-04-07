7a2eb3643f6edf66e678a0dd34d92ca668374597
Meidad <90615374+Meidad1@users.noreply.github.com>
2022-04-07 11:43:07 +0300

    Created a new project

--- Autopus/web_page_scraper.py
+++ Autopus/web_page_scraper.py
@@ -1,2 +1,27 @@
 class WebPageScraper(object):
     pass
+
+
+import requests, bs4
+
+
+
+def retrieve_html_from_url(url):
+    """
+
+    :param url: str
+    :return: html
+    """
+
+
+def parse_html(html):
+    """
+
+    :param html:
+    :return: dictionary with the information with the fields as keys (title, date etc)
+    """
+    pass
+
+
+
+
