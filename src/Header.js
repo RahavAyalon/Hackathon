48a62df48fbc535472286f5bfdbb1fafbb24c97e
Rahav Ayalon <rayalon@synamedia.com>
2022-04-07 15:22:51 +0300

    some progress in UI

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Autopus</div>;
+  return (
+    <>
+      <div>Autopus</div>
+      <input type="text" />
+      <button>Sky News</button>
+      <button>The Guardian</button>
+      <button>Al Jazeera</button>
+    </>
+  );
 }
