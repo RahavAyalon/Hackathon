9ec5a9b24919b68389654013f8074ba8d8d519f5
Shir Ehrlich <shir.ehrlich@mail.huji.ac.il>
2022-04-07 17:10:48 +0300

    deleted spare functions

--- Autopus/autopus_backend_manager.py
+++ Autopus/autopus_backend_manager.py
@@ -12,13 +12,18 @@ class CrawlerManager:
         self.number_of_pages = number_of_pages
 
     def create_minimized_articles_list(self, original_articles_list):
+        """
+        :param original_articles_list: list of articles- each is a json dict with ALL the field extracted
+        :return: list of lists with only the relevant feilds from each article
+        """
         minimized = []
         for article_json in original_articles_list:
-            article_list = [0] * len(self.required_fields_dict)
+            article_list = [0] * len(self.required_fields_dict)  # creates "empty" list with the size of the minimized list
             for field, i in self.required_fields_dict.items():
-                article_list[i] = article_json[field]
+                article_list[i] = article_json["fields"][field]  # relevant in the guardian json
             minimized.append(article_list)
         return minimized
+
 
     def write_to_excel(self):
         pass
