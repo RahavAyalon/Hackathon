526a45bb7c7786130bd492bfd7111c7698e2e9f5
Rahav Ayalon <rayalon@synamedia.com>
2022-04-07 16:14:05 +0300

    some progress in UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Logo from './Logo';
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Logo />
+
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import image from './logo.png';
+
+export default function logo() {
+  return <img src={image} height={100} width={100} />;
+}
