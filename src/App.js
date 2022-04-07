8abfcb27159ce87cc60180b7e098975e8dd136a5
Rahav Ayalon <rayalon@synamedia.com>
2022-04-07 23:16:51 +0300

    vfduhchdsjk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Filtering from './Filtering';
 import Main from './Main';
+import Data from './Data';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './Navbar';
@@ -10,6 +11,7 @@ function App() {
       <div>
         <NavbarComponent />
         <Routes>
+          <Route exact path="/data" element={<Data />} />
           <Route exact path="/filtering" element={<Filtering />} />
           <Route exact path="/" element={<Main />} />
         </Routes>
