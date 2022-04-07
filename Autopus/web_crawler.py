31361d47ff4145a2d67457ef44520eaade600d0c
Shir Ehrlich <shir.ehrlich@mail.huji.ac.il>
2022-04-07 16:49:47 +0300

    crawler maybe done

--- Autopus/web_crawler.py
+++ Autopus/web_crawler.py
@@ -43,3 +43,40 @@ class WebCrawler(object):
             self.get_articles_from_cur_page()
             self.next_page()
         return self.articles
+
+import web_crawler
+
+NUMBER_OF_PAGES = 5              # the user decides
+ARTICLES_PER_PAGE = 200
+GUARDIAN_API_KEY = "92ecbb3d-1602-42b3-acdb-e0a9d166e354"
+GUARDIAN_API_ENDPOINT = 'http://content.guardianapis.com/search'
+
+
+# USER_REQUIRED_FIELDS_LIST = ["headline", "webPublicationDate"]
+
+
+class CrawlerManager:
+
+    def __init__(self, required_fields_list):
+        self.required_fields_dict = {field: i for i, field in enumerate(required_fields_list)}
+
+    def create_minimized_articles_list(self, original_articles_list):
+        minimized = []
+        for article_json in original_articles_list:
+            article_list = [0] * len(self.required_fields_dict)
+            for field, i in self.required_fields_dict.items():
+                article_list[i] = article_json[field]
+            minimized.append(article_list)
+        return minimized
+
+
+
+    def manage_crawling(self):
+        crawler = web_crawler.WebCrawler(GUARDIAN_API_ENDPOINT, GUARDIAN_API_KEY, ARTICLES_PER_PAGE,
+                              NUMBER_OF_PAGES)
+        articles_list = crawler.run()                # a list of dictionaries
+        minimized_list = self.create_minimized_articles_list(articles_list)
+
+
+
+
