3d36adc4f8e2b583eb73a9b5752846a283773e1b
Shir Ehrlich <shir.ehrlich@mail.huji.ac.il>
2022-04-08 03:42:09 +0300

    organizing files

--- Autopus/local.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def parse_article_body(self, article_body):
-    """
-    :param article_body: text of the body of a certain article
-    :return: the required separated str
-    """
-    pattern = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!|\")\s"
-    return re.split(pattern, article_body)
